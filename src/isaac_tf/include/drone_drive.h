fe11310986e253bbccf941b9a538a70e1b60d576
EnderMandS <952025764@qq.com>
2024-01-20 13:23:14 +0000

    camera_pose tf

--- src/isaac_tf/include/drone_drive.h
+++ src/isaac_tf/include/drone_drive.h
@@ -2,16 +2,20 @@
 #define __DRONE_DRIVE_H
 
 #include "drone_msgs/PositionCommand.h"
+#include "ros/publisher.h"
 #include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <nav_msgs/Odometry.h>
 
 namespace Drone {
 
 class DroneDrive {
 public:
-  DroneDrive(){};
+  DroneDrive():o_c_listener(tf_buffer){};
   void init(ros::NodeHandle &);
 
 private:
+  // drone drive
   ros::Subscriber posititon_cmd_sub;
   ros::Publisher joint_pub;
   ros::Timer test_timer;
@@ -19,7 +23,15 @@ private:
   drone_msgs::PositionCommand posititon_cmd;
 
   void positionCommandCb(const drone_msgs::PositionCommandConstPtr &);
-  void testTimerCb(const ros::TimerEvent&);
+  void testTimerCb(const ros::TimerEvent &);
+
+  // camera pose tf
+  ros::Subscriber odom_sub;
+  ros::Publisher camera_pose_pub;
+  tf2_ros::Buffer tf_buffer;
+  tf2_ros::TransformListener o_c_listener;
+
+  void odomCb(const nav_msgs::OdometryConstPtr &);
 };
 
 } // namespace Drone
--- src/isaac_tf/src/drone_drive.cpp
+++ src/isaac_tf/src/drone_drive.cpp
@@ -1,4 +1,6 @@
 #include "drone_drive.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "ros/time.h"
 #include <cmath>
 #include <sensor_msgs/JointState.h>
 
@@ -19,8 +21,10 @@ void DroneDrive::init(ros::NodeHandle &nh) {
   posititon_cmd_sub =
       nh.subscribe("/position_cmd", 5, &DroneDrive::positionCommandCb, this);
   joint_pub = nh.advertise<sensor_msgs::JointState>("/drone_cmd", 10);
-  test_timer =
-      nh.createTimer(ros::Duration(0.1), &DroneDrive::testTimerCb, this);
+  odom_sub = nh.subscribe("/odom", 1, &DroneDrive::odomCb, this);
+  camera_pose_pub = nh.advertise<geometry_msgs::PoseStamped>("/camera_pose", 5);
+  // test_timer =
+  //     nh.createTimer(ros::Duration(0.1), &DroneDrive::testTimerCb, this);
 }
 
 void DroneDrive::positionCommandCb(
@@ -29,17 +33,37 @@ void DroneDrive::positionCommandCb(
 
   sensor_msgs::JointState joint_cmd;
   joint_cmd.header.stamp = ros::Time::now();
-  joint_cmd.name = {"x_joint","y_joint","z_joint","R_body"};
-  joint_cmd.position = {msg->position.x, msg->position.y, msg->position.z, msg->yaw};
+  joint_cmd.name = {"x_joint", "y_joint", "z_joint", "R_body"};
+  joint_cmd.position = {msg->position.x, msg->position.y, msg->position.z,
+                        msg->yaw};
   joint_pub.publish(joint_cmd);
 }
 
 void DroneDrive::testTimerCb(const ros::TimerEvent &e) {
   static auto t_start = e.current_real.toSec();
-  auto t = e.current_real.toSec()-t_start;
+  auto t = e.current_real.toSec() - t_start;
   sensor_msgs::JointState joint_cmd;
   joint_cmd.header.stamp = ros::Time::now();
-  joint_cmd.name = {"x_joint","y_joint","z_joint","R_body"};
-  joint_cmd.position = {t * 0.05, t * 0.05, t * 0.05, sin(t)*M_PI};
+  joint_cmd.name = {"x_joint", "y_joint", "z_joint", "R_body"};
+  joint_cmd.position = {t * 0.05, t * 0.05, t * 0.05, sin(t) * M_PI};
   joint_pub.publish(joint_cmd);
+}
+
+void DroneDrive::odomCb(const nav_msgs::OdometryConstPtr &msg) {
+  geometry_msgs::TransformStamped tf_stamped;
+  try {
+    tf_stamped = tf_buffer.lookupTransform("odom", "camera", ros::Time(0));
+  } catch (tf2::TransformException &e) {
+    ROS_WARN("%s", e.what());
+    return;
+  }
+
+  geometry_msgs::PoseStamped camera_pose;
+  camera_pose.pose.orientation = tf_stamped.transform.rotation;
+  camera_pose.pose.position.x = tf_stamped.transform.translation.x;
+  camera_pose.pose.position.y = tf_stamped.transform.translation.y;
+  camera_pose.pose.position.z = tf_stamped.transform.translation.z;
+  camera_pose.header.frame_id = tf_stamped.child_frame_id;
+  camera_pose.header.stamp = ros::Time::now();
+  camera_pose_pub.publish(camera_pose);
 }
