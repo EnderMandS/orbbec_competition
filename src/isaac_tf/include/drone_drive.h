abffbcf7625762d67223fda44368411fef1fac61
EnderMandS <952025764@qq.com>
2024-01-20 11:54:53 +0000

    drive drone test success

--- src/isaac_tf/include/drone_drive.h
+++ src/isaac_tf/include/drone_drive.h
@@ -14,10 +14,12 @@ public:
 private:
   ros::Subscriber posititon_cmd_sub;
   ros::Publisher joint_pub;
+  ros::Timer test_timer;
 
   isaac_tf::PositionCommand posititon_cmd;
 
   void positionCommandCb(const isaac_tf::PositionCommandConstPtr &);
+  void testTimerCb(const ros::TimerEvent&);
 };
 
 } // namespace Drone
--- src/isaac_tf/src/drone_drive.cpp
+++ src/isaac_tf/src/drone_drive.cpp
@@ -1,4 +1,5 @@
 #include "drone_drive.h"
+#include <cmath>
 #include <sensor_msgs/JointState.h>
 
 int main(int argc, char **argv) {
@@ -7,7 +8,7 @@ int main(int argc, char **argv) {
 
   Drone::DroneDrive drone_drive;
   drone_drive.init(nh);
-
+  ROS_INFO("DroneDrive start.");
   ros::spin();
   return 0;
 }
@@ -16,9 +17,10 @@ using namespace Drone;
 
 void DroneDrive::init(ros::NodeHandle &nh) {
   posititon_cmd_sub =
-      nh.subscribe("/position_cmd", 1, &DroneDrive::positionCommandCb, this);
+      nh.subscribe("/position_cmd", 5, &DroneDrive::positionCommandCb, this);
   joint_pub = nh.advertise<sensor_msgs::JointState>("/drone_cmd", 10);
-  ROS_INFO("DroneDrive init.");
+  test_timer =
+      nh.createTimer(ros::Duration(0.1), &DroneDrive::testTimerCb, this);
 }
 
 void DroneDrive::positionCommandCb(
@@ -27,7 +29,17 @@ void DroneDrive::positionCommandCb(
 
   sensor_msgs::JointState joint_cmd;
   joint_cmd.header.stamp = ros::Time::now();
-  joint_cmd.name = {"base_link"};
-  joint_cmd.position = {msg->position.x,msg->position.y,msg->position.z};
+  joint_cmd.name = {"x_joint","y_joint","z_joint","R_body"};
+  joint_cmd.position = {msg->position.x, msg->position.y, msg->position.z, msg->yaw};
+  joint_pub.publish(joint_cmd);
+}
+
+void DroneDrive::testTimerCb(const ros::TimerEvent &e) {
+  static auto t_start = e.current_real.toSec();
+  auto t = e.current_real.toSec()-t_start;
+  sensor_msgs::JointState joint_cmd;
+  joint_cmd.header.stamp = ros::Time::now();
+  joint_cmd.name = {"x_joint","y_joint","z_joint","R_body"};
+  joint_cmd.position = {t * 0.05, t * 0.05, t * 0.05, sin(t)*M_PI};
   joint_pub.publish(joint_cmd);
 }
